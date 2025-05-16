b45a5c8275f7ebe8864ad9ac2cc6504b0e96fcb8
yurimjin <jayjinjay@gmail.com>
2025-05-16 16:19:31 +0900

    feat: 나머지 3개 탭에도 bottomnav 넣기

--- src/views/reading-log/ui/ReadingLogPage.tsx
+++ src/views/reading-log/ui/ReadingLogPage.tsx
@@ -1,14 +1,15 @@
 import { ReadingLogList } from '@/src/features/article/ui/ReadingLogList';
-import { FullPageCentered } from '@/src/shared/ui/layout';
+import { BottomNav } from '@/src/widgets/bottom-nav';
 import { Suspense } from 'react';
 
 export function ReadingLogPage() {
   return (
-    <FullPageCentered>
+    <>
       <h1 className="text-2xl font-bold mb-8">Reading log</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <ReadingLogList />
       </Suspense>
-    </FullPageCentered>
+      <BottomNav />
+    </>
   );
 }
